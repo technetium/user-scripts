d6592f2ad8574ac8b131f6ee6c9c40d2fc433efb
technetium <github@technetium.be>
2025-04-04 17:59:01 +0200

    Update osmdirections.user.js
    Use listener to get reference, even if it's added after loading

--- osmdirections.user.js
+++ osmdirections.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OSM Directions
 // @namespace    http://technetium.be
-// @version      1.3
+// @version      1.4
 // @description  Replaces the links to GoogleDirections to OpenStreetMap directions
 // @author       Toni Cornelissen (github@technetium.be)
 // @match        *://*.geocaching.com/geocache/*
@@ -11,13 +11,15 @@
     'use strict';
 
     function main() {
-        window.addEventListener('load', function() {
-            document.querySelectorAll('a[href^="https://maps.google.com/maps\\?f=d"]').forEach((elem) => {
-                const orig = elem.href.replace(/.*saddr=/, '').replace(/%20.*/, '');
-                const dest = elem.href.replace(/.*daddr=/, '').replace(/%20.*/, '');
-                elem.href = 'https://www.openstreetmap.org/directions?route='+orig+'%3B'+dest;
+        window.addEventListener('click', function(e) {
+            if ('A' != e.target.tagName) { return; }
+            if (e.target.href.startsWith('https://maps.google.com/maps?f=d')) {
+                e.preventDefault();
+                const orig = e.target.href.replace(/.*saddr=/, '').replace(/%20.*/, '');
+                const dest = e.target.href.replace(/.*daddr=/, '').replace(/%20.*/, '');
+                location.href = 'https://www.openstreetmap.org/directions?route='+orig+'%3B'+dest;
+            }
           });
-        }, false);
     }
     main();
 })();
