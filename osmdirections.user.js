0597b9457ca3bd0b215770a60324fcd84eb43d62
technetium <github@technetium.be>
2026-06-23 13:22:59 +0200

    More google links in OSM Directions

--- osmdirections.user.js
+++ osmdirections.user.js
@@ -1,25 +1,49 @@
 // ==UserScript==
 // @name         OSM Directions
 // @namespace    http://technetium.be
-// @version      1.4
+// @version      1.6
 // @description  Replaces the links to GoogleDirections to OpenStreetMap directions
-// @author       Toni Cornelissen (github@technetium.be)
-// @match        *://*.geocaching.com/geocache/*
+// @author       Toni Cornelissen (codeberg@technetium.be)
+// @include      *
+// @downloadURL https://update.greasyfork.org/scripts/582982/OSM%20Directions.user.js
+// @updateURL https://update.greasyfork.org/scripts/582982/OSM%20Directions.meta.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     function main() {
+        console.log("OSM Directions");
         window.addEventListener('click', function(e) {
-            if ('A' != e.target.tagName) { return; }
-            if (e.target.href.startsWith('https://maps.google.com/maps?f=d')) {
+            let target = e.target;
+            while (('A' !== target.tagName) && target.parentElement) {
+                target = target.parentElement;
+            }
+            if ('A' != target.tagName) {
+            	return;
+            }
+            let orig = '';
+            let dest = '';
+			let query = '';
+            if (target.href.startsWith('https://maps.google.com/maps?f=d')) {
+                orig = target.href.replace(/.*saddr=/, '').replace(/%20.*/, '');
+                dest = target.href.replace(/.*daddr=/, '').replace(/%20.*/, '');
+            } else if (target.href.startsWith('https://www.google.com/maps/dir/?api=1')) {
+                dest = target.href.replace(/.*destination=/, '').replace(/&.*/, '');
+            } else if (target.href.startsWith('https://www.google.com/maps?q=')) {
+				query = target.href.replace(/.*\?q=/, '');
+				console.log('QUERY: ',	query);
+            } 
+            if (dest) {
                 e.preventDefault();
-                const orig = e.target.href.replace(/.*saddr=/, '').replace(/%20.*/, '');
-                const dest = e.target.href.replace(/.*daddr=/, '').replace(/%20.*/, '');
                 location.href = 'https://www.openstreetmap.org/directions?route='+orig+'%3B'+dest;
             }
-          });
+			if (query) {
+                e.preventDefault();
+                location.href = 'https://www.openstreetmap.org?query='+query;
+			}
+        });
     }
     main();
 })();
+
